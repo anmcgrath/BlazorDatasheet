5aefa3fcd4b79bb28c719d9453ff0a9e1550e8fa
anmcgrath <andrew.n.mcgrath@gmail.com>
2022-08-14 14:57:45 +1000

    Change Cell.RawValue to Cell.Value

--- src/BlazorDatasheet/Model/Cell.cs
+++ src/BlazorDatasheet/Model/Cell.cs
@@ -3,6 +3,6 @@ namespace BlazorDatasheet.Model;
 public class Cell
 {
     public string Type { get; set; } = "text";
-    public string? RawValue { get; set; }
+    public string? Value { get; set; }
     public string Format { get; set; } = "";
 }
