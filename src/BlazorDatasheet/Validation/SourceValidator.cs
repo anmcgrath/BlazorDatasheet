039678ec5b5b8889f4ce94da81d3ff3dddd5e811
anmcgrath <andrew.n.mcgrath@gmail.com>
2022-09-06 11:03:45 +1000

    Update example

--- src/BlazorDatasheet/Validation/SourceValidator.cs
+++ src/BlazorDatasheet/Validation/SourceValidator.cs
@@ -15,13 +15,10 @@ public class SourceValidator : IDataValidator
 
     public bool IsValid(object val)
     {
-        Console.WriteLine("Validating " + val);
         try
         {
             var valStr = val.ToString();
-            Console.WriteLine("Comparing " + valStr + " to " + string.Join(",", ValidationSource));
             var isValid = ValidationSource.Any(x => x.CompareTo(valStr) == 0);
-            Console.WriteLine(isValid);
             return isValid;
         }
         catch (Exception e)
