3708f345122bca138c4305c6ca039dc5ed689c77
anmcgrath <andrew.n.mcgrath@gmail.com>
2022-10-23 10:41:31 +1100

    Move row rendering outside of cell rendering virtual container.

--- src/BlazorDatasheet/Data/Events/RowMouseEvent.cs
+++ src/BlazorDatasheet/Data/Events/RowMouseEvent.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+
 namespace BlazorDatasheet.Data.Events;
 
 public class RowMouseEvent
 {
-    public int RowIndex { get; set; }
+    public RowMouseEvent(int rowIndex, MouseEventArgs mouseEventArgs)
+    {
+        RowIndex = rowIndex;
+        MouseEventArgs = mouseEventArgs;
+    }
+
+    public int RowIndex { get; }
+    public MouseEventArgs MouseEventArgs { get; }
 }
