75eb8107f88acacf3c941fcf4cad8c4dd76e3ad9
anmcgrath <andrew.n.mcgrath@gmail.com>
2023-06-06 13:16:20 +1000

    Remove extra un-used event args for the editor.

--- src/BlazorDatasheet/Datasheet.razor.cs
+++ src/BlazorDatasheet/Datasheet.razor.cs
@@ -2,7 +2,6 @@ using System.Text;
 using BlazorDatasheet.Commands;
 using BlazorDatasheet.Data;
 using BlazorDatasheet.Edit;
-using BlazorDatasheet.Edit.Events;
 using BlazorDatasheet.Events;
 using BlazorDatasheet.Formats;
 using BlazorDatasheet.Interfaces;
