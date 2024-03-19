76a0ca059d99fe3d3d66ba0df2357145b8a802af
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-03-19 18:47:10 +1100

    Add js input service

---
+++ src/BlazorDatasheet/Events/SheetPointerEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorDatasheet.Events;
+
+public class SheetPointerEventArgs
+{
+    public int Row { get; set; }
+    public int Col { get; set; }
+    public double SheetX { get; set; }
+    public double SheetY { get; set; }
+    public bool AltKey { get; set; }
+    public bool CtrlKey { get; set; }
+    public bool ShiftKey { get; set; }
+    public bool MetaKey { get; set; }
+}
