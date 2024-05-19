a9df48ce2dafba5e51626c007e0fc1ab4f49a636
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-05-19 12:51:09 +1000

    Add comparison between date and number

---
+++ test/BlazorDatasheet.Test/CellValues/CellValueTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using BlazorDatasheet.Formula.Core;
+using BlazorDatasheet.Formula.Core.Extensions;
+using FluentAssertions;
+
+namespace BlazorDatasheet.Test.CellValues;
+
+public class CellValueTests
+{
+    [Test]
+    public void Cell_Value_Date_Compares_With_Number()
+    {
+        var c1 = CellValue.Date(1.0.ToDate());
+        var c2 = CellValue.Number(0);
+        var c3 = CellValue.Date((-1.0).ToDate());
+
+        c2.CompareTo(c1).Should().Be(-1);
+        c2.CompareTo(c3).Should().Be(1);
+    }
+}
