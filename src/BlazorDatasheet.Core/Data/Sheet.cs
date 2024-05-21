ebf765eec2732b41c47f178ff8337ce31439596d
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-05-21 21:43:03 +1000

    Update default styles

--- src/BlazorDatasheet.Core/Data/Sheet.cs
+++ src/BlazorDatasheet.Core/Data/Sheet.cs
@@ -114,7 +114,7 @@ public class Sheet
         Selection = new Selection(this);
         Editor = new Editor(this);
         Validators = new ValidationManager(this);
-        Rows = new RowInfoStore(25, this);
+        Rows = new RowInfoStore(24, this);
         Columns = new ColumnInfoStore(105, this);
         FormulaEngine = new FormulaEngine.FormulaEngine(this);
         ConditionalFormats = new ConditionalFormatManager(this, Cells);
