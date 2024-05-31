b81654fe3a846a7f5f5485024fbe0b2c6eb7df22
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-05-31 20:16:44 +1000

    Add stylebuilder extension for adding non-null values

--- src/BlazorDatasheet/Util/StyleBuilder.cs
+++ src/BlazorDatasheet/Util/StyleBuilder.cs
@@ -18,6 +18,13 @@ public class StyleBuilder
         return this;
     }
 
+    public StyleBuilder AddStyleNotNull(string name, string? value)
+    {
+        if (value != null)
+            _stringBuilder.Append($"{name}:{value};");
+        return this;
+    }
+
     public StyleBuilder AddStyle(string name, string value)
     {
         _stringBuilder.Append($"{name}:{value};");
