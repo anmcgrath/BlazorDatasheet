5ab25e92477ef0023b966e93c59003200e37f4a2
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-06-01 15:27:48 +1000

    Use style builder

--- src/BlazorDatasheet/Util/CssUtil.cs
+++ src/BlazorDatasheet/Util/CssUtil.cs
@@ -14,34 +14,17 @@ public static class CssUtil
     /// <returns></returns>
     public static string GetStyledInput(IReadOnlyCell cell)
     {
-        var str = new StringBuilder();
-        if (cell?.Format?.BackgroundColor == null)
-            str.Append("background:var(--sheet-bg-color);");
+        var sb = new StyleBuilder();
+        if (cell?.Format?.BackgroundColor != null)
+            sb.AddStyle("background", cell.Format.BackgroundColor);
         else
-            str.Append($"background:{cell.Format.BackgroundColor};");
+            sb.AddStyle("background", "var(--sheet-bg-color)");
 
         if (cell?.Format?.ForegroundColor == null)
-            str.Append("color:var(--sheet-foreground-color)");
+            sb.AddStyle("color", "var(--sheet-foreground-color)");
         else
-            str.Append($"color:{cell.Format.ForegroundColor};");
+            sb.AddStyle("color", cell.Format.ForegroundColor);
 
-        return str.ToString();
-    }
-
-    /// <summary>
-    /// Returns the css strings for producing width & max width of a cell given its location and span
-    /// </summary>
-    /// <param name="col"></param>
-    /// <param name="colSpan"></param>
-    /// <param name="provider"></param>
-    /// <returns></returns>
-    public static string GetCellWidthStyles(int col, int colSpan, CellLayoutProvider provider)
-    {
-        var width = provider.ComputeWidth(col, colSpan);
-        var str = new StringBuilder();
-        str.Append($"width:{width}px;");
-        str.Append($"max-width:{width}px;");
-        str.Append($"min-width:{width}px;");
-        return str.ToString();
+        return sb.ToString();
     }
 }
