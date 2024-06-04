bac98c54faa9a75d127ee950c6e345f1b4e5ffe6
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-06-04 14:51:55 +1000

    Link portals to datasheet

--- src/BlazorDatasheet/Portal/PortalService.cs
+++ src/BlazorDatasheet/Portal/PortalService.cs
@@ -4,36 +4,37 @@ namespace BlazorDatasheet.Portal;
 
 public class PortalService
 {
-    private Dictionary<string, List<RenderFragment>> _portalTargets = new();
-    private Dictionary<string, PortalTarget> _portalTargetComponents = new();
+    private readonly Dictionary<(string id, string dataSheetId), List<RenderFragment>> _portalTargets = new();
+    private readonly Dictionary<(string id, string dataSheetId), PortalTarget> _portalTargetComponents = new();
 
-    public void RegisterPortalTarget(string id ,PortalTarget target)
+    public void RegisterPortalTarget(string id, string datasheetId, PortalTarget target)
     {
-        _portalTargets.TryAdd(id, new List<RenderFragment>());
-        _portalTargetComponents.TryAdd(id, target);
+        _portalTargets.TryAdd((id, datasheetId), new List<RenderFragment>());
+        _portalTargetComponents.TryAdd((id, datasheetId), target);
     }
 
-    public void AddToPortal(string portalTargetId, RenderFragment fragment)
+    public void AddToPortal(string portalTargetId, string datasheetId, RenderFragment fragment)
     {
-        if (_portalTargets.TryGetValue(portalTargetId, out var fragments))
+        Console.WriteLine($"Adding fragment to portal {portalTargetId} for datasheet {datasheetId}");
+        if (_portalTargets.TryGetValue((portalTargetId, datasheetId), out var fragments))
         {
             fragments.Add(fragment);
-            
-            if(_portalTargetComponents.TryGetValue(portalTargetId, out var target))
+
+            if (_portalTargetComponents.TryGetValue((portalTargetId, datasheetId), out var target))
                 target.SetRenderFragments(fragments);
         }
     }
 
-    public IEnumerable<RenderFragment> GetFragments(string portalTargetId)
+    public IEnumerable<RenderFragment> GetFragments(string portalTargetId, string datasheetId)
     {
-        return _portalTargets.TryGetValue(portalTargetId, out var fragments)
+        return _portalTargets.TryGetValue((portalTargetId, datasheetId), out var fragments)
             ? fragments
             : Enumerable.Empty<RenderFragment>();
     }
 
-    public void RenderPortalTarget(string id)
+    public void RenderPortalTarget(string id, string datasheetId)
     {
-        if(_portalTargetComponents.TryGetValue(id, out var target))
+        if (_portalTargetComponents.TryGetValue((id, datasheetId), out var target))
             target.Render();
     }
 }
