dd0dcb9087a09cfb45a0d02b054f918a2414f946
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-06-30 19:30:10 +1000

    Remove context menu from inputs

--- src/BlazorDatasheet/wwwroot/blazor-datasheet.js
+++ src/BlazorDatasheet/wwwroot/blazor-datasheet.js
@@ -1,5 +1,3 @@
-
-
 window.writeTextToClipboard = async function (text) {
     await window.navigator.clipboard.writeText(text)
 }
@@ -8,4 +6,15 @@ window.setFocusWithTimeout = function (el, timeout) {
     setTimeout(() => {
         el.focus()
     }, timeout)
+}
+
+window.setContextListener = function (el, dotnetHelper, handlerName) {
+    el.addEventListener('contextmenu', async e => {
+        console.log(e.target)
+        if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA' || e.target.hasAttribute("contentEditable"))
+            return
+
+        e.preventDefault()
+        await dotnetHelper.invokeMethodAsync(handlerName, {clientX: e.clientX, clientY: e.clientY})
+    })
 }
