7de3efdc55da619c0c3c5fa9391fb1c87adcc7c6
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-11-10 15:09:37 +1100

    Proof of concept of frozen rows/columns working without editing.

--- src/BlazorDatasheet/DatasheetCssGrid.razor.cs
+++ src/BlazorDatasheet/DatasheetCssGrid.razor.cs
@@ -66,9 +66,40 @@ public partial class DatasheetCssGrid : SheetComponentBase
     /// Specifies how many columns are frozen on the left side of the grid.
     /// </summary>
     [Parameter]
-    public int FrozenLeftCount { get; set; } = 0;
+    public int FrozenLeftCount { get; set; }
 
-    private int _frozenLeftCount = 0;
+    private int _frozenLeftCount;
+
+    /// <summary>
+    /// Specifies how many columns are frozen on the right side of the grid.
+    /// </summary>
+    [Parameter]
+    public int FrozenRightCount { get; set; }
+
+    private int _frozenRightCount;
+
+    /// <summary>
+    /// Specifies how many rows are frozen on the top side of the grid.
+    /// </summary>
+    [Parameter]
+    public int FrozenTopCount { get; set; }
+
+    private int _frozenTopCount;
+
+    /// <summary>
+    /// Specifies how many rows are frozen on the bottom side of the grid.
+    /// </summary>
+    [Parameter]
+    public int FrozenBottomCount { get; set; }
+
+    private int _frozenBottomCount;
+    
+    /// <summary>
+    /// An indicator of how deep the grid is. Any sub-grid of the grid should have a higher <see cref="GridLevel"/> than its parent.
+    /// This is used internally and should not be used in most circumstances.
+    /// </summary>
+    [Parameter]
+    public int GridLevel { get; set; }
 
     /// <summary>
     /// Register custom editor components (derived from <see cref="BaseEditor"/>) that will be selected
@@ -89,12 +120,20 @@ public partial class DatasheetCssGrid : SheetComponentBase
     [Parameter]
     public bool Virtualise { get; set; } = true;
 
-    [Parameter] 
-    public bool StickyHeaders { get; set; } = true;
+    [Parameter] public bool StickyHeaders { get; set; } = true;
+
+    /// <summary>
+    /// The size of the main region of this datasheet, that is the region of the grid without
+    /// any frozen rows or columns.
+    /// </summary>
+    private Region MainViewRegion => new(
+        Math.Max(FrozenTopCount, _viewRegion.Top),
+        Math.Min(_viewRegion.Bottom - _frozenBottomCount, _viewRegion.Bottom),
+        Math.Max(FrozenLeftCount, _viewRegion.Left),
+        Math.Min(_viewRegion.Right - _frozenRightCount, _viewRegion.Right));
 
     protected override Task OnParametersSetAsync()
     {
-
         if (Sheet != _sheet)
         {
             RemoveEvents(_sheet);
@@ -105,14 +144,17 @@ public partial class DatasheetCssGrid : SheetComponentBase
         if (ViewRegion != _viewRegion)
         {
             _viewRegion = ViewRegion ?? _sheet.Region;
-            StateHasChanged();
         }
 
-        if (_frozenLeftCount != FrozenLeftCount)
+        if (_frozenLeftCount != FrozenLeftCount || _frozenRightCount != FrozenRightCount)
         {
             _frozenLeftCount = FrozenLeftCount;
-            StateHasChanged();
+            _frozenRightCount = FrozenRightCount;
+            _frozenBottomCount = FrozenBottomCount;
+            _frozenTopCount = FrozenTopCount;
         }
+
+        StateHasChanged();
 
         return base.OnParametersSetAsync();
     }
@@ -125,12 +167,10 @@ public partial class DatasheetCssGrid : SheetComponentBase
 
     private void RemoveEvents(Sheet sheet)
     {
-        
     }
 
     private void AddEvents(Sheet sheet)
     {
-        
     }
 
     private Type GetCellRendererType(string type)
