05093e30e66ca9d0e5f4e38e6393dceb859c6be9
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-11-23 21:13:37 +1100

    Fix render of all hidden rows/columns.

--- test/BlazorDatasheet.Test/Store/RowColInfoStoreTests.cs
+++ test/BlazorDatasheet.Test/Store/RowColInfoStoreTests.cs
@@ -7,11 +7,19 @@ namespace BlazorDatasheet.Test.Store;
 public class RowColInfoStoreTests
 {
     [Test]
-    public void Hidden_Row_Cols_Calculates_Correctly()
+    public void Hidden_Row_Cols_Calculates_Correctly_When_Size_Is_one()
     {
         var sheet = new Sheet(100, 100);
         sheet.Rows.HideImpl(0, 100);
         sheet.Rows.CountVisible(0, 0).Should().Be(0);
         sheet.Rows.GetVisibleIndices(0, 0).Should().BeEmpty();
+    }
+
+    [Test]
+    public void Hidden_Row_Cols_Calculate_Correctly_When_Size_Query_Larger_Than_Sheet()
+    {
+        var sheet = new Sheet(100, 100);
+        sheet.Rows.HideImpl(0, 100);
+        sheet.Rows.CountVisible(0, 1000).Should().Be(0);
     }
 }
