d9431b64df8ed3f7f5c226dc9e39f48e858054cf
anmcgrath <34253568+anmcgrath@users.noreply.github.com>
2024-11-25 10:39:10 +0000

    Deploying to gh-pages from  @ 3342ead0eff6ba157aa3cb103e0d98cbf46664a7 🚀

--- _content/BlazorDatasheet/blazor-datasheet.js
+++ _content/BlazorDatasheet/blazor-datasheet.js
@@ -1,5 +1,10 @@
 window.writeTextToClipboard = async function (text) {
-    await window.navigator.clipboard.writeText(text)
+    if (window.isSecureContext) {
+        await window.navigator.clipboard.writeText(text)
+    } else {
+        console.log("Copy failed as window was not considered a secure context.")
+    }
+
 }
 
 window.setFocusWithTimeout = function (el, timeout) {
