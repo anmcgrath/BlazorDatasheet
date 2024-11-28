b450c9c38e6e30a6c0ba85d40631d7c50f3defe7
anmcgrath <andrew.n.mcgrath@gmail.com>
2024-11-28 16:09:46 +1100

    Input editor work

--- src/BlazorDatasheet/wwwroot/js/highlighter.js
+++ src/BlazorDatasheet/wwwroot/js/highlighter.js
@@ -15,21 +15,29 @@ class Highligher {
         this.#inputEl.addEventListener('input', e => {
             if (!options.dotnetHelper)
                 return
+            console.log('input set', e.target.innerText)
+            console.log('caret position = ', this.getCaretPosition())
             options.dotnetHelper.invokeMethodAsync("UpdateInput", e.target.innerText)
         })
 
-
-        this.updateCaretPosition = function () {
+        ///Returns caret position if selection is inside highligher, otherwise -1
+        this.getCaretPosition = function () {
             let sel = window.getSelection()
+            if(sel == null || sel.focusNode == null)
+                return -1
+            
             let isSelectionInside = sel.focusNode.parentElement === options.inputEl
-            console.log('updateCaretPosition, inside? ', isSelectionInside)
             if (!isSelectionInside)
-                return
+                return -1
             let len = sel.toString().length
             let caretPosition = -1
             if (len === 0)
                 caretPosition = sel.focusOffset
-            options.dotnetHelper.invokeMethodAsync("UpdateCaretPosition", caretPosition)
+            return caretPosition
+        }
+
+        this.updateCaretPosition = function () {
+            options.dotnetHelper.invokeMethodAsync("UpdateCaretPosition", this.getCaretPosition())
         }
 
         const moveCursorToEnd = function (el) {
@@ -50,7 +58,7 @@ class Highligher {
             setTimeout(this.focusAndMoveCursorToEnd, 0);
         }
 
-        document.addEventListener('selectionchange', this.updateCaretPosition)
+        //document.addEventListener('selectionchange', this.updateCaretPosition)
     }
 
 
@@ -58,8 +66,51 @@ class Highligher {
         this.#highlightResultEl.innerHTML = html
     }
 
+    createRange = (node, targetPosition) => {
+        let range = document.createRange();
+        range.selectNode(node);
+        range.setStart(node, 0);
+
+        let pos = 0;
+        const stack = [node];
+        while (stack.length > 0) {
+            const current = stack.pop();
+
+            if (current.nodeType === Node.TEXT_NODE) {
+                const len = current.textContent.length;
+                if (pos + len >= targetPosition) {
+                    range.setEnd(current, targetPosition - pos);
+                    return range;
+                }
+                pos += len;
+            } else if (current.childNodes && current.childNodes.length > 0) {
+                for (let i = current.childNodes.length - 1; i >= 0; i--) {
+                    stack.push(current.childNodes[i]);
+                }
+            }
+        }
+
+        // The target position is greater than the
+        // length of the contenteditable element.
+        range.setEnd(node, node.childNodes.length);
+        return range;
+    };
+
     setInnerText(text) {
+        const cursorPosition = this.getCaretPosition()
         this.#inputEl.innerText = text;
+
+        if(cursorPosition < 0)
+            return
+        
+        const setrange = this.createRange(this.#inputEl, cursorPosition);
+
+        const setselection = window.getSelection();
+        setrange.collapse(false)
+        console.log(setrange)
+        setselection.removeAllRanges();
+        setselection.addRange(setrange);
+        
     }
 
     dispose() {
