2b786ea8ad25df7ff284cd45e23d08062292f931
anmcgrath <34253568+anmcgrath@users.noreply.github.com>
2025-04-05 08:59:35 +0000

    Deploying to gh-pages from  @ c912f562e54b7a315dec7a7b10530de7860fffa8 🚀

---
+++ _content/BlazorDatasheet/Render/Layers/AutofitLayer.razor.js
@@ -0,0 +1,21 @@
+﻿/**
+ *
+ * @param {HTMLElement} el
+ */
+export function measureMaxChildrenDimensions(el) {
+    if (el) {
+        let results = []
+
+        for (let child of el.children) {
+            results.push({
+                size: {
+                    width: child.offsetWidth,
+                    height: child.offsetHeight
+                },
+                row: parseInt(child.dataset.row),
+                col: parseInt(child.dataset.col),
+            })
+        }
+        return results
+    }
+}
