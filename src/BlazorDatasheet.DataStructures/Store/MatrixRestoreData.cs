5c583c730fa5686119326c25093fc1231a4b90a3
anmcgrath <andrew.n.mcgrath@gmail.com>
2025-04-13 09:11:31 +1000

    Don't use empty array if not necessary.

--- src/BlazorDatasheet.DataStructures/Store/MatrixRestoreData.cs
+++ src/BlazorDatasheet.DataStructures/Store/MatrixRestoreData.cs
@@ -3,11 +3,17 @@ namespace BlazorDatasheet.DataStructures.Store;
 public class MatrixRestoreData<T>
 {
     public List<(int row, int col, T? data)> DataRemoved { get; internal set; } = new();
-    public List<AppliedShift> Shifts { get; internal set; } = new();
+    public List<AppliedShift>? Shifts { get; internal set; }
 
     public void Merge(MatrixRestoreData<T> item)
     {
         DataRemoved.AddRange(item.DataRemoved);
-        Shifts.AddRange(item.Shifts);
+        if (item.Shifts != null)
+        {
+            if (Shifts == null)
+                Shifts = item.Shifts;
+            else
+                Shifts.AddRange(item.Shifts);
+        }
     }
 }
