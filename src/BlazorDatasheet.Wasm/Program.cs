9d7693836222775da6add95c6eba7034e3ca68f0
anmcgrath <andrew.n.mcgrath@gmail.com>
2025-04-15 21:20:54 +1000

    Fix blazor server not routing correctly.

--- src/BlazorDatasheet.Wasm/Program.cs
+++ src/BlazorDatasheet.Wasm/Program.cs
@@ -1,11 +1,12 @@
 using BlazorDatasheet.Extensions;
+using BlazorDatasheet.SharedPages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorDatasheet.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddBlazorDatasheet();
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<AppWasm>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
