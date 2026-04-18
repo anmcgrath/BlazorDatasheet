922b92a4f54545f77283a0f7d75894923a3e6dc8
anmcgrath <34253568+anmcgrath@users.noreply.github.com>
2026-04-18 10:25:38 +1000

    Improve number scale colour formats

--- src/BlazorDatasheet.Core/Color/ColorConverter.cs
+++ src/BlazorDatasheet.Core/Color/ColorConverter.cs
@@ -111,4 +111,66 @@ public class ColorConverter
         double v = Math.Clamp(((1 - t) * c0.v + t * c1.v), 0, 1);
         return (h, s, v);
     }
+
+    public static (double l, double a, double b) RgbToOklab(System.Drawing.Color color)
+    {
+        double r = SrgbToLinear(color.R / 255.0);
+        double g = SrgbToLinear(color.G / 255.0);
+        double b = SrgbToLinear(color.B / 255.0);
+
+        double l = 0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b;
+        double m = 0.2119034982 * r + 0.6740817638 * g + 0.1140147380 * b;
+        double s = 0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b;
+
+        double l_ = Math.Cbrt(l);
+        double m_ = Math.Cbrt(m);
+        double s_ = Math.Cbrt(s);
+
+        return (
+            0.2104542553 * l_ + 0.7936177850 * m_ - 0.0040720468 * s_,
+            1.9779984951 * l_ - 2.4285922050 * m_ + 0.4505937099 * s_,
+            0.0259040371 * l_ + 0.7827717662 * m_ - 0.8086757660 * s_
+        );
+    }
+
+    public static System.Drawing.Color OklabToRgb(double l, double a, double b)
+    {
+        double l_ = l + 0.3963377774 * a + 0.2158037573 * b;
+        double m_ = l - 0.1055613458 * a - 0.0638541728 * b;
+        double s_ = l - 0.0894841775 * a - 1.2914855480 * b;
+
+        double l_lin = l_ * l_ * l_;
+        double m_lin = m_ * m_ * m_;
+        double s_lin = s_ * s_ * s_;
+
+        double rVal = +4.0767416621 * l_lin - 3.3077115913 * m_lin + 0.2309699292 * s_lin;
+        double gVal = -1.2684380046 * l_lin + 2.6097574011 * m_lin - 0.3413193965 * s_lin;
+        double bVal = -0.0041960863 * l_lin - 0.7034186147 * m_lin + 1.7076147010 * s_lin;
+
+        return System.Drawing.Color.FromArgb(
+            (byte)Math.Clamp(LinearToSrgb(rVal) * 255, 0, 255),
+            (byte)Math.Clamp(LinearToSrgb(gVal) * 255, 0, 255),
+            (byte)Math.Clamp(LinearToSrgb(bVal) * 255, 0, 255)
+        );
+    }
+
+    public static (double l, double a, double b) OklabInterp((double l, double a, double b) c0,
+        (double l, double a, double b) c1, double t)
+    {
+        return (
+            (1 - t) * c0.l + t * c1.l,
+            (1 - t) * c0.a + t * c1.a,
+            (1 - t) * c0.b + t * c1.b
+        );
+    }
+
+    private static double SrgbToLinear(double x)
+    {
+        return x <= 0.04045 ? x / 12.92 : Math.Pow((x + 0.055) / 1.055, 2.4);
+    }
+
+    private static double LinearToSrgb(double x)
+    {
+        return x <= 0.0031308 ? 12.92 * x : 1.055 * Math.Pow(x, 1 / 2.4) - 0.055;
+    }
 }
