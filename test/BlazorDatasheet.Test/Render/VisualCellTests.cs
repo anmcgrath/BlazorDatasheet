c6d56312e8f9ce4a1344f0277496b24e930d837d
anmcgrath <andrew.n.mcgrath@gmail.com>
2026-08-15 21:12:17 +1000

    Fix some rendering bugs to improve re-renders on parameter changes.

--- test/BlazorDatasheet.Test/Render/VisualCellTests.cs
+++ test/BlazorDatasheet.Test/Render/VisualCellTests.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorDatasheet.Core.Data;
 using BlazorDatasheet.Core.Formats;
+using BlazorDatasheet.DataStructures.Geometry;
 using BlazorDatasheet.Render;
 using FluentAssertions;
 using NUnit.Framework;
@@ -19,5 +20,53 @@ public class VisualCellTests
         };
         var vc = new VisualCell(0, 0, sheet, 12);
         vc.FormatStyleString.Should().Contain("text-wrap");
+    }
+
+    [Test]
+    public void Merged_Cell_Dimensions_Include_Visible_Cells_After_Hidden_Internal_Row_And_Column()
+    {
+        var sheet = new Sheet(3, 3);
+        sheet.Rows.SetSize(0, 20);
+        sheet.Rows.SetSize(1, 30);
+        sheet.Rows.SetSize(2, 40);
+        sheet.Columns.SetSize(0, 50);
+        sheet.Columns.SetSize(1, 60);
+        sheet.Columns.SetSize(2, 70);
+        sheet.Cells.Merge(new Region(0, 2, 0, 2));
+        sheet.Rows.Hide(1, 1);
+        sheet.Columns.Hide(1, 1);
+
+        var cell = new VisualCell(0, 0, sheet, 12);
+
+        cell.VisibleMergeRowStart.Should().Be(0);
+        cell.VisibleMergeColStart.Should().Be(0);
+        cell.VisibleRowSpan.Should().Be(2);
+        cell.VisibleColSpan.Should().Be(2);
+        cell.Height.Should().Be(60);
+        cell.Width.Should().Be(120);
+    }
+
+    [Test]
+    public void Merged_Cell_Dimensions_Use_First_Visible_Row_And_Column_When_Leading_Ones_Are_Hidden()
+    {
+        var sheet = new Sheet(3, 3);
+        sheet.Rows.SetSize(0, 20);
+        sheet.Rows.SetSize(1, 30);
+        sheet.Rows.SetSize(2, 40);
+        sheet.Columns.SetSize(0, 50);
+        sheet.Columns.SetSize(1, 60);
+        sheet.Columns.SetSize(2, 70);
+        sheet.Cells.Merge(new Region(0, 2, 0, 2));
+        sheet.Rows.Hide(0, 1);
+        sheet.Columns.Hide(0, 1);
+
+        var cell = new VisualCell(0, 0, sheet, 12);
+
+        cell.VisibleMergeRowStart.Should().Be(1);
+        cell.VisibleMergeColStart.Should().Be(1);
+        cell.VisibleRowSpan.Should().Be(2);
+        cell.VisibleColSpan.Should().Be(2);
+        cell.Height.Should().Be(70);
+        cell.Width.Should().Be(130);
     }
 }
