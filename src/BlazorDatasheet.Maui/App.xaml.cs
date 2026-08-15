0e8ad4143fc3668aee8a535f9edcc186267d5444
anmcgrath <34253568+anmcgrath@users.noreply.github.com>
2026-08-15 19:26:34 +1000

    Add MAUI example

---
+++ src/BlazorDatasheet.Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorDatasheet.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "BlazorDatasheet.Maui" };
+    }
+}
