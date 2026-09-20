84f7f16a05f31e497ec5fcd4e6958ea7a008b125
anmcgrath <andrew.n.mcgrath@gmail.com>
2026-09-20 15:56:05 +1000

    Show function and parameter descriptions in the function suggestions and hint box.

--- test/BlazorDatasheet.Test/Formula/FunctionRegistryTests.cs
+++ test/BlazorDatasheet.Test/Formula/FunctionRegistryTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using BlazorDatasheet.Core.Data;
 using BlazorDatasheet.Formula.Core;
 using NUnit.Framework;
 
@@ -59,5 +61,36 @@ public class FunctionRegistryTests
 
         Assert.That(descriptor.MinArity, Is.EqualTo(1));
         Assert.That(descriptor.MaxArity, Is.EqualTo(128));
+    }
+
+    [Test]
+    public void Descriptions_Are_Optional_And_Kept()
+    {
+        var plain = new FunctionDescriptor("PLAIN", [new ParameterDefinition("x", ParameterType.Number)],
+            (_, _) => CellValue.Number(1));
+        var described = new FunctionDescriptor(
+            "DESCRIBED",
+            [new ParameterDefinition("x", ParameterType.Number, description: "A number.")],
+            (_, _) => CellValue.Number(1),
+            description: "Does something.");
+
+        Assert.That(plain.Description, Is.Null);
+        Assert.That(plain.ParameterDefinitions[0].Description, Is.Null);
+        Assert.That(described.Description, Is.EqualTo("Does something."));
+        Assert.That(described.ParameterDefinitions[0].Description, Is.EqualTo("A number."));
+    }
+
+    [Test]
+    public void Built_In_Functions_And_Their_Parameters_Are_Described()
+    {
+        var functions = Workbook.BuildDefaultRegistry(options: null).SearchForFunctions("").ToList();
+
+        Assert.That(functions, Is.Not.Empty);
+        foreach (var function in functions)
+        {
+            Assert.That(function.Function.Description, Is.Not.Null.And.Not.Empty, function.Name);
+            foreach (var parameter in function.Function.ParameterDefinitions)
+                Assert.That(parameter.Description, Is.Not.Null.And.Not.Empty, $"{function.Name} {parameter.Name}");
+        }
     }
 }
