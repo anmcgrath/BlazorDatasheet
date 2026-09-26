0e6cc0da608e4a3bdc4aa4233a125161b75b3099
anmcgrath <andrew.n.mcgrath@gmail.com>
2026-09-26 21:05:36 +1000

    Simplify shared cell border setting

--- test/BlazorDatasheet.Test/Format/Cell_Format_Tests.cs
+++ test/BlazorDatasheet.Test/Format/Cell_Format_Tests.cs
@@ -6,6 +6,41 @@ namespace BlazorDatasheet.Test.Format;
 
 public class Cell_Format_Tests
 {
+    [TestCase(nameof(CellFormat.BorderLeft))]
+    [TestCase(nameof(CellFormat.BorderRight))]
+    [TestCase(nameof(CellFormat.BorderTop))]
+    [TestCase(nameof(CellFormat.BorderBottom))]
+    public void Partial_Border_Merge_Preserves_Unspecified_Properties_And_Source_Formats(string side)
+    {
+        var property = typeof(CellFormat).GetProperty(side)!;
+        var original = new CellFormat();
+        var originalBorder = new Border { Width = 2, Color = "red" };
+        property.SetValue(original, originalBorder);
+        var merged = original.Clone();
+        var incoming = new CellFormat();
+        var incomingBorder = new Border { Color = "blue" };
+        property.SetValue(incoming, incomingBorder);
+
+        merged.Merge(incoming);
+        var result = (Border)property.GetValue(merged)!;
+        result.Width.Should().Be(2);
+        result.Color.Should().Be("blue");
+        result.Should().NotBeSameAs(originalBorder);
+        result.Should().NotBeSameAs(incomingBorder);
+        originalBorder.Color.Should().Be("red");
+        incomingBorder.Width.Should().BeNull();
+
+        property.SetValue(incoming, new Border { Width = 3 });
+        merged.Merge(incoming);
+        result = (Border)property.GetValue(merged)!;
+        result.Width.Should().Be(3);
+        result.Color.Should().Be("blue");
+
+        property.SetValue(incoming, null);
+        merged.Merge(incoming);
+        property.GetValue(merged).Should().BeNull();
+    }
+
     [Test]
     public void Compare_Cell_Formats_With_Same_Formats_Is_True()
     {
